6fe6e268f14bc4d6be6ef52f38eef02e2f2529c9
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-01-23 18:49:49 +0330

    create components layout

---
+++ src/components/SelectList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SelectList = () => {
+  return (
+    <div class='movie-container'>
+      <label>Pick a movie: </label>
+      <select id='select-list'>
+        <option value='10'>Interstaller ($10)</option>
+        <option value='15'>Scare Face ($15)</option>
+        <option value='8'>Good Fellas ($8)</option>
+        <option value='20'>God Father ($20)</option>
+      </select>
+    </div>
+  );
+};
+
+export default SelectList;
