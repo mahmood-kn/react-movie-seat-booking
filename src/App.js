4f19c458b8db299762d996f4fe9691038accb661
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-01-23 18:57:56 +0330

    screen component init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import SelectList from './components/SelectList';
 import Help from './components/Help';
+import Screen from './components/Screen';
 
 function App() {
   return (
     <>
       <SelectList />
       <Help />
+      <div className='container'>
+        <Screen />
+      </div>
     </>
   );
 }
